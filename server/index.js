c19e9462e27a3680e89eaf0f3ad717a9a777cb92
zhangling <zl2@winbaoxian.com>
2018-03-12 23:03:35 +0800

    feat(增加模版引擎中间件):

--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
 const Koa = require('Koa')
 const app = new Koa()
-const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
-const ejs = require('ejs')
-const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html; charset=utf-8'
-  // ctx.body = ejs.render(ejsTpl, {
-  //   you: 'Luke',
-  //   me: 'Scott'
-  // })
-  ctx.body = pug.render(pugTpl, {
+  await ctx.render('index', {
     you: 'Luke',
-    me: 'Scott'
+    me: 'LingF'
   })
 })
 app.listen(4455)
